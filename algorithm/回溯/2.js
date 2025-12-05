3e441403c8b907105c5a2eaa343ff6a3778687fb
Aijavai <17807080178@163.com>
2025-12-05 22:21:14 +0800

    子集

--- algorithm/回溯/2.js
+++ algorithm/回溯/2.js
@@ -1,2 +1,22 @@
 // 子集
-// 树 不能回溯
+// 树 不能重复 从startIndex 开始循环
+
+function B(nums) {
+    const result = [];
+    const path = [];
+
+    function backtrack(startIndex) {
+        result.push([...path]);
+
+        for (let i = startIndex, n = nums.length; i < n; i++) {
+            path.push(nums[i]);
+
+            backtrack(i + 1);
+            
+            path.pop();
+        }
+    }
+
+    backtrack(0);
+    return result;
+}
